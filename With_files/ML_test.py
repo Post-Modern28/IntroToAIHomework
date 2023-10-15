2d7961f0a3658aa802180f29b070d049b825ea17
test <jovesfun@gmail.com>
2023-10-15 20:03:32 +0300

    idk

--- With_files/ML_test.py
+++ With_files/ML_test.py
@@ -1,9 +1,31 @@
 import numpy as np
 import pandas as pd
-import scipy
-import sklearn
+# import scipy
+# import sklearn
+# import plotly
+# import statistics
+# from collections import Counter
+import math
+import matplotlib as mpl
 from matplotlib import pyplot as plt
+from sklearn.metrics import *
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import normalize
+
+pd.set_option("display.notebook_repr_html", False)
+pd.set_option("display.max_columns", 8)
+pd.set_option("display.max_rows", 10)
+pd.set_option("display.width", 80)
+
+# arr = np.array([[1]*10**6+[10**6]*3])
+#
+# print(np.linalg.norm(arr))
+# print(arr[0][-1:-5:-1])
+# arr = normalize(arr)
+#
+# arr = arr[abs(arr-arr.mean()) < 3*arr.std()]
+# print(arr[0][-1:-5:-1])
 
 
-def f(x: int) -> int:
-    return x * 2
+
+
